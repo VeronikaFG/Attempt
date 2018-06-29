3e34601b3ecbf667964182b712406db2e3da6184
Veronica Giannotti <verogiann@libero.it>
2018-06-29 17:33:25 +0200

    Update bookshelf function

--- src/BookLibrary.js
+++ src/BookLibrary.js
@@ -6,4 +6,22 @@ const allCategories = ["currentlyReading", "wantToRead", "read"]
 
 function BookLibrary(props) {
   const { books, changeShelf } = props
-  
+
+  return (
+    <div>
+      {allCategories.map(shelf => (
+        <div key={shelf} className="bookshelf">
+          <h2 className="bookshelf-title">{shelf}</h2>
+          <div className="bookshelf-books">
+            <BooksList
+              books={books.filter(
+                book => book.shelf === shelf
+              )}
+              changeShelf={changeShelf}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
