e4a69f22f7ee526dc284b75933fd909ff46aa088
Veronica Giannotti <verogiann@libero.it>
2018-06-29 17:25:18 +0200

    Initialize react components

--- src/BookLibrary.js
+++ src/BookLibrary.js
@@ -1,1 +1,9 @@
-uoooooooo
+import React from "react"
+import PropTypes from "prop-types"
+import BooksList from "./BooksList"
+
+const allCategories = ["currentlyReading", "wantToRead", "read"]
+
+function BookLibrary(props) {
+  const { books, changeShelf } = props
+  
