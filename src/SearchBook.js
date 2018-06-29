b013bdb284a7f51d08ff99a8b6cb4bab3ed53b10
Veronica Giannotti <verogiann@libero.it>
2018-06-29 18:07:31 +0200

    Initialize class SearchBook

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,2 +1,15 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
+import BooksAPI from "./BooksAPI"
+import BooksList from "./BooksList"
+import PropTypes from "prop-types"
+
+class SearchBook extends Component {
+  static propTypes = {
+    categorizedBook: PropTypes.func.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  }
+
+  state = {
+    results: []
+  }
