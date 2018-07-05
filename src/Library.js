b7008ffb6de5720cd0183ba0b1708f757b6bc229
Veronica Giannotti <verogiann@libero.it>
2018-07-05 19:44:46 +0200

    update code

--- src/Library.js
+++ src/Library.js
@@ -8,3 +8,46 @@ class Library extends React.Component {
   resetQuery() {
     this.setState({query: ""});
   }
+
+  render() {
+    // Destructure props object
+    const { state, update, search } = this.props;
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search"
+            onClick={this.resetQuery}>
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            {/* Search by title or author */}
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={state.query}
+              onChange={event => search(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          {/*Search books selected by user */}
+          <SearchBook
+            books={state.books}
+            userBooks={state.userBooks}
+            query={state.query}
+            update={update}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+Library.propTypes = {
+  state: PropTypes.object.isRequired,
+  update: PropTypes.func,
+  search: PropTypes.func
+};
+
+export default Library;
