1d284b67fc067ee3168f16bda996d72c92004237
Veronica Giannotti <verogiann@libero.it>
2018-07-05 19:47:53 +0200

    Update code

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -20,3 +20,63 @@ class SearchBook extends React.Component {
         }
       });
     }
+
+    return (
+      <ol className="books-grid">
+        {visualizeBooks.map(Book => (
+          <li key={Book.id}>
+            <div className="book">
+              <div className="book-top">
+
+                {Book.imageLinks && (
+                  <img src={Book.imageLinks.thumbnail}
+                    className="book-cover"
+                    alt={Book.title}
+                    style={{
+											width: 128,
+											height: 192
+										}}
+                  />
+                )}
+
+                <div className="book-shelf-changer">
+                  {/* If a book is not categorized, set category "none" */}
+                  <select
+                    value={Book.shelf || "none"}
+                    id={Book.id}
+                    onChange={event => update(Book, event.target.value)}
+                  >
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{Book.title}</div>
+              {/*  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.*/}
+              {Book.authors &&
+                Book.authors.map(Bookauthor => (<div className="book-authors"
+                  key={Bookauthor}></div>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+SearchBook.propTypes = {
+	query: PropTypes.object.isRequired,
+  update: PropTypes.func,
+	books: PropTypes.array.isRequired,
+  userBooks: PropTypes.object.isRequired
+};
+
+export default SearchBook;
