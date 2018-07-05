ad8118354aecca9254633ba37f24c2ab47e0f32a
Veronica Giannotti <verogiann@libero.it>
2018-07-05 19:41:57 +0200

    render all shelves

--- src/BookLibrary.js
+++ src/BookLibrary.js
@@ -12,3 +12,66 @@ class BookLibrary extends React.Component {
     let currentShelf = state.books.filter(book => book.shelf === "currentlyReading");
     let WantReadShelf = state.books.filter(book => book.shelf === "wantToRead");
     let ReadShelf = state.books.filter(book => book.shelf === "read");
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {currentShelf.length > 0 && (
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <SortBook
+                    books={currentShelf}
+                    update={update}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+          <div>
+            {WantReadShelf.length > 0 && (
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                  <SortBook
+                    books={WantReadShelf}
+                    update={update}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+          <div>
+            {ReadShelf.length > 0 && (
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                  <SortBook
+                    books={ReadShelf}
+                    update={update}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div>
+          <Link to="/search" className="open-search open-link">
+            Add a book
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+BookLibrary.propTypes = {
+  state: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+  update: PropTypes.func
+};
+
+export default BookLibrary;
